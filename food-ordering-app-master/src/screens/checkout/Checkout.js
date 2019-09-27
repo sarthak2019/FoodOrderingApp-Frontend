a73243ab98be59c068fe5c3da2798b305a4b9bd9
chetan-rc <chetan4195@gmail.com>
2019-09-27 12:40:39 +0530

    errors in stepper template

---
+++ food-ordering-app-master/src/screens/checkout/Checkout.js
@@ -0,0 +1,382 @@
+import React, { Component } from 'react';
+import './Checkout.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import FormControl from '@material-ui/core/FormControl';
+import Typography from '@material-ui/core/Typography';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import StepContent from '@material-ui/core/StepContent';
+import Paper from '@material-ui/core/Paper';
+import { Redirect } from 'react-router';
+
+{/*
+getSteps = () => {
+    return ['Select campaign settings', 'Create an ad group', 'Create an ad'];
+}
+
+getStepContent = (step) => {
+    switch (step) {
+        case 0:
+            return `For each ad campaign that you create, you can control how much
+              you're willing to spend on clicks and conversions, which networks
+              and geographical locations you want your ads to show on, and more.`;
+        case 1:
+            return 'An ad group contains one or more ads which target a shared set of keywords.';
+        case 2:
+            return `Try out different ad text to see what brings in the most customers,
+              and learn how to enhance your ads using features like ad extensions.
+              If you run into any problems with your ads, find out how to tell if
+              they're running and how to resolve approval issues.`;
+        default:
+            return 'Unknown step';
+    }
+} */}
+
+function getSteps() {
+    return ['Delivery', 'Payment'];
+}
+
+function getStepContent(step) {
+    switch (step) {
+        case 0:
+            return `For each ad campaign that you create, you can control how much
+              you're willing to spend on clicks and conversions, which networks
+              and geographical locations you want your ads to show on, and more.`;
+        case 1:
+            return 'An ad group contains one or more ads which target a shared set of keywords.';
+        case 2:
+            return `Try out different ad text to see what brings in the most customers,
+              and learn how to enhance your ads using features like ad extensions.
+              If you run into any problems with your ads, find out how to tell if
+              they're running and how to resolve approval issues.`;
+        default:
+            return 'Unknown step';
+    }
+}
+
+
+class Checkout extends Component {
+    
+
+    render() {
+        //const classes = useStyles();
+        const [activeStep, setActiveStep] = React.useState(0);
+        const steps = getSteps();
+
+        const handleNext = () => {
+            setActiveStep(prevActiveStep => prevActiveStep + 1);
+        };
+
+        const handleBack = () => {
+            setActiveStep(prevActiveStep => prevActiveStep - 1);
+        };
+
+        const handleReset = () => {
+            setActiveStep(0);
+        }; 
+
+
+        return (
+
+            <div>
+                <Stepper activeStep={activeStep} orientation="vertical">
+                    {steps.map((label, index) => (
+                        <Step key={label}>
+                            <StepLabel>{label}</StepLabel>
+                            <StepContent>
+                                <Typography>{getStepContent(index)}</Typography>
+                                {/*<div className={classes.actionsContainer}>*/}
+                                 <div>
+                                    <div>
+                                        <Button
+                                            disabled={activeStep === 0}
+                                            onClick={handleBack}
+                                            
+                                        >
+                                            Back
+                                        </Button>
+                                        <Button
+                                            variant="contained"
+                                            color="primary"
+                                            onClick={handleNext}
+                                  
+                                        >
+                                            {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                                        </Button>
+                                    </div>
+                                </div>
+                            </StepContent>
+                        </Step>
+                    ))}
+                </Stepper>
+                
+            </div>
+
+        )
+
+    }
+}
+
+export default Checkout;
+{/*
+const useStyles = theme => ({
+    root: {
+        width: '90%',
+    },
+    button: {
+        marginTop: theme.spacing(1),
+        marginRight: theme.spacing(1),
+    },
+    actionsContainer: {
+        marginBottom: theme.spacing(2),
+    },
+    resetContainer: {
+        padding: theme.spacing(3),
+    },
+});
+
+function getSteps() {
+    return ['Select campaign settings', 'Create an ad group', 'Create an ad'];
+}
+
+function getStepContent(step) {
+    switch (step) {
+        case 0:
+            return `For each ad campaign that you create, you can control how much
+              you're willing to spend on clicks and conversions, which networks
+              and geographical locations you want your ads to show on, and more.`;
+        case 1:
+            return 'An ad group contains one or more ads which target a shared set of keywords.';
+        case 2:
+            return `Try out different ad text to see what brings in the most customers,
+              and learn how to enhance your ads using features like ad extensions.
+              If you run into any problems with your ads, find out how to tell if
+              they're running and how to resolve approval issues.`;
+        default:
+            return 'Unknown step';
+    }
+}
+
+class Checkout extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            usernameRequired: "dispNone",
+            passwordRequired: "dispNone",
+            invalidCredentials: "dispNone",
+            username: "",
+            password: "",
+            loggedIn: sessionStorage.getItem("access-token") == null ? false : true,
+            redirect: false
+        }
+    }
+
+    inputUsernameChangeHandler = (e) => {
+        this.setState({ username: e.target.value });
+    }
+
+    inputPasswordChangeHandler = (e) => {
+        this.setState({ password: e.target.value });
+    }
+
+    loginClickHandler = () => {
+        this.setState({ invalidCredentials: "dispNone" });
+        this.state.username === "" ? this.setState({ usernameRequired: "dispBlock" }) : this.setState({ usernameRequired: "dispNone" });
+        this.state.password === "" ? this.setState({ passwordRequired: "dispBlock" }) : this.setState({ passwordRequired: "dispNone" });
+        var username = "user";
+        var password = "password";
+        if (this.state.username !== "" && this.state.password !== "") {
+            if (this.state.username === username && this.state.password === password) {
+                sessionStorage.setItem("access-token", "18621945434.69f451e.ba20db8552f241eabbaa87e804f73169");
+                this.setState({ redirect: true })
+            }
+            else {
+                this.setState({ invalidCredentials: "dispBlock" });
+            }
+        }
+    }
+
+   
+
+
+    render() {
+    //const classes = useStyles();
+    const [activeStep, setActiveStep] = React.useState(0);
+    const steps = getSteps();
+
+    const handleNext = () => {
+        setActiveStep(prevActiveStep => prevActiveStep + 1);
+    };
+
+    const handleBack = () => {
+        setActiveStep(prevActiveStep => prevActiveStep - 1);
+    };
+
+    const handleReset = () => {
+        setActiveStep(0);
+    };
+
+
+        return (
+            
+                <div>
+                <Stepper activeStep={activeStep} orientation="vertical">
+                    {steps.map((label, index) => (
+                        <Step key={label}>
+                            <StepLabel>{label}</StepLabel>
+                            <StepContent>
+                                <Typography>{getStepContent(index)}</Typography>
+                                {/*<div className={classes.actionsContainer}>*/}
+{/*
+                                 <div>
+                                    <div>
+                                        <Button
+                                            disabled={activeStep === 0}
+                                            onClick={handleBack}
+                                            
+                                        >
+                                            Back
+                                        </Button>
+                                        <Button
+                                            variant="contained"
+                                            color="primary"
+                                            onClick={handleNext}
+                                  
+                                        >
+                                            {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                                        </Button>
+                                    </div>
+                                </div>
+                            </StepContent>
+                        </Step>
+                    ))}
+                </Stepper>
+                
+            </div>
+
+        )
+
+    }
+}
+
+export default Checkout;
+/*}
+//export default makeStyles(useStyles)(Checkout); 
+
+/*
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import StepContent from '@material-ui/core/StepContent';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        width: '90%',
+    },
+    button: {
+        marginTop: theme.spacing(1),
+        marginRight: theme.spacing(1),
+    },
+    actionsContainer: {
+        marginBottom: theme.spacing(2),
+    },
+    resetContainer: {
+        padding: theme.spacing(3),
+    },
+}));
+
+function getSteps() {
+    return ['Select campaign settings', 'Create an ad group', 'Create an ad'];
+}
+
+function getStepContent(step) {
+    switch (step) {
+        case 0:
+            return `For each ad campaign that you create, you can control how much
+              you're willing to spend on clicks and conversions, which networks
+              and geographical locations you want your ads to show on, and more.`;
+        case 1:
+            return 'An ad group contains one or more ads which target a shared set of keywords.';
+        case 2:
+            return `Try out different ad text to see what brings in the most customers,
+              and learn how to enhance your ads using features like ad extensions.
+              If you run into any problems with your ads, find out how to tell if
+              they're running and how to resolve approval issues.`;
+        default:
+            return 'Unknown step';
+    }
+}
+
+export default function VerticalLinearStepper() {
+    const classes = useStyles();
+    const [activeStep, setActiveStep] = React.useState(0);
+    const steps = getSteps();
+
+    const handleNext = () => {
+        setActiveStep(prevActiveStep => prevActiveStep + 1);
+    };
+
+    const handleBack = () => {
+        setActiveStep(prevActiveStep => prevActiveStep - 1);
+    };
+
+    const handleReset = () => {
+        setActiveStep(0);
+    };
+
+    return (
+        <div className={classes.root}>
+            <Stepper activeStep={activeStep} orientation="vertical">
+                {steps.map((label, index) => (
+                    <Step key={label}>
+                        <StepLabel>{label}</StepLabel>
+                        <StepContent>
+                            <Typography>{getStepContent(index)}</Typography>
+                            <div className={classes.actionsContainer}>
+                                <div>
+                                    <Button
+                                        disabled={activeStep === 0}
+                                        onClick={handleBack}
+                                        className={classes.button}
+                                    >
+                                        Back
+                  </Button>
+                                    <Button
+                                        variant="contained"
+                                        color="primary"
+                                        onClick={handleNext}
+                                        className={classes.button}
+                                    >
+                                        {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                                    </Button>
+                                </div>
+                            </div>
+                        </StepContent>
+                    </Step>
+                ))}
+            </Stepper>
+            {activeStep === steps.length && (
+                <Paper square elevation={0} className={classes.resetContainer}>
+                    <Typography>All steps completed - you&apos;re finished</Typography>
+                    <Button onClick={handleReset} className={classes.button}>
+                        Reset
+          </Button>
+                </Paper>
+            )}
+        </div>
+    );
+} */}
