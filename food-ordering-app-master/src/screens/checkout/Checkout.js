775de3e9082e53f18d091e3a680b94d3d618108f
chetan-rc <chetan4195@gmail.com>
2019-09-28 11:36:46 +0530

    Steps added

--- food-ordering-app-master/src/screens/checkout/Checkout.js
+++ food-ordering-app-master/src/screens/checkout/Checkout.js
@@ -36,11 +36,7 @@ function getStepContent(step) {
               and geographical locations you want your ads to show on, and more.`;
         case 1:
             return 'An ad group contains one or more ads which target a shared set of keywords.';
-        case 2:
-            return `Try out different ad text to see what brings in the most customers,
-              and learn how to enhance your ads using features like ad extensions.
-              If you run into any problems with your ads, find out how to tell if
-              they're running and how to resolve approval issues.`;
+        
         default:
             return 'Unknown step';
     }
